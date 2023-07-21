9168cdedc7840371b948948a26e449abb1ee9dc7
Madina <madina-khat@yandex.ru>
2023-07-21 17:19:34 +0300

    Курсовая__2

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,33 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static Employee[] employees() {
-        Employee[] employee = new Employee[10];
-        employee[0] = new Employee("Петров", "Петр", "Петрович", 1, 32000);
-        employee[1] = new Employee("Смирнов", "Семен", "Семенович", 2, 42000);
-        return employee;
-    }
-
-    static Employee[] employee = employees();
-
-    public static void listOfAllEmployees() {
-        System.out.println("Список сотрудников:");
-        for (Employee list : employee) {
-            if (list == null) {
-                break;
-            }
-            System.out.println(list);
+        public static int[] removeDuplicates(int[] arr) {
+            return Arrays.stream(arr).distinct().toArray();
         }
-    }
 
-    public static void salaryExpensesPerMonth() {
-        float sum = 0;
-        for (Employee i : employee) {
-            if (i == null) {
-                break;
-            }
-            sum += i.salary;
-        }
-        System.out.println("Сумма затрат на зарплаты в месяц: " + sum);
-    }
+        public static void main(String[] args) {
+            int[] arr = {2, 4, 1, 2, 1, 2, 4, 5};
 
-    public static void minimumSalaryEmployee() {
-        float minSalary = 1_000_000;
-        String fullName = null;
-        int department = 0;
-        int id = 0;
-        for (Employee i : employee) {
-            if (i == null) {
-                break;
-            } else if (i.salary < minSalary) {
-                minSalary = i.salary;
-                fullName = i.fullName;
-                department = i.department;
-                id = i.id;
-            }
-        }
-        System.out.println('\n' + "Минимальная зарплата у сотрудника №" + id + '\n' + "ФИО: " +
-                fullName + '\n' + "Отдел: " + department + '\n' + "Зарплата: " + minSalary);
+            int[] distinct = removeDuplicates(arr);
+            System.out.println(Arrays.toString(distinct));
 
-    }
+        //-//-//
 
-    public static void main(String[] args) {
-        listOfAllEmployees();
-        salaryExpensesPerMonth();
-        minimumSalaryEmployee();
-    }
+
+    List<String> names = Arrays.asList("John", "Mary", "Peter", "Alice");
+
+    List<String> mNames = names.stream()
+            .map(name -> name.substring(1)) // удаление первой буквы из каждого имени
+
+            .sorted() // сортировка списка
+
+            .collect(Collectors.toList());
+
+
+    System.out.println(mNames); // [Alice, ohn, ary, eter]
 }
+}
+
+
